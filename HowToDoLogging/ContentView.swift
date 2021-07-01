b53aa68e3ceb9667e630badb2a24eaa1afdcabb8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-07-01 11:52:04 +0300

    Initial Commit

---
+++ HowToDoLogging/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowToDoLogging
+//
+//  Created by ramil on 01.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HowToDoLogging/HowToDoLoggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToDoLoggingApp.swift
+//  HowToDoLogging
+//
+//  Created by ramil on 01.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToDoLoggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
