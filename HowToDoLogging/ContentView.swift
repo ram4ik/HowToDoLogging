09a47463ac7f627b44ef29383ee754b457243b60
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-07-01 12:13:01 +0300

    init

--- HowToDoLogging/ContentView.swift
+++ HowToDoLogging/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+        }.onAppear() {
+            Log.info("Slow network connectivity detected")
+            Log.warning("Username should not be nil")
+            Log.error("User authentication failed")
+        }
     }
 }
 
---
+++ HowToDoLogging/Log.swift
@@ -0,0 +1,64 @@
+//
+//  Log.swift
+//  HowToDoLogging
+//
+//  Created by ramil on 01.07.2021.
+//
+
+import Foundation
+
+enum Log {
+    enum LogLevel {
+        case info
+        case warning
+        case error
+        
+        fileprivate var prefix: String {
+            switch self {
+            case .info:
+                return "INFO ℹ️"
+            case .warning:
+                return "WARN ⚠️"
+            case .error:
+                return "ALERT 🚨"
+            }
+        }
+    }
+    
+    struct Context {
+        let file: String
+        let function: String
+        let line: Int
+        var description: String {
+            return "\((file as NSString).lastPathComponent):\(line) \(function)"
+        }
+    }
+    
+    static func info(_ str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .info, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+    static func warning(_ str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .warning, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+    static func error(_ str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .error, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+    fileprivate static func handleLog(level: LogLevel, str: String, shouldLogContext: Bool, context: Context) {
+        let logComponents = ["[\(level.prefix)]", str]
+        
+        var fullString = logComponents.joined(separator: " ")
+        if shouldLogContext {
+            fullString += " ➝ \(context.description)"
+        }
+        
+        #if DEBUG
+        print(fullString)
+        #endif
+    }
+}
